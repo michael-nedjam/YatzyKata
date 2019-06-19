e09d7c9ebc49719c948a83978dbc7bb802111b5b
michael nedjam <michael.nedjam@gmail.com>
2019-06-19 22:36:01 +0200

    Introduce roll concept

---
+++ src/DiceRoll.java
@@ -0,0 +1,49 @@
+
+public class DiceRoll {
+	private int[] diceValue;
+	private int numberDiceSide = 6;
+	
+	public DiceRoll(int ... diceValue) {
+		this.diceValue = diceValue;
+	}	
+	
+	public int sumDiceValue() {
+		int sum = 0;
+		for (int dieValue : diceValue) {
+			sum+=dieValue;
+		}
+		return sum;
+	}
+	
+	public int numberOf(int value) {
+		int number = 0;
+		for (int dieValue : diceValue) {
+			if(dieValue == value)
+				number++;
+		}
+		return number;
+	}
+	
+	public Integer findHighestMatchingDice(int numberOfDice) {
+		int[] numberOfValue = listNumberOfValue(this.diceValue, this.numberDiceSide);
+		return findLastIndexForMinimunValue(numberOfValue, numberOfDice);
+	}
+	
+	private Integer findLastIndexForMinimunValue(int[] values, int minimunValue) {
+		Integer lastIndex = null;
+		for(int i = values.length-1; i > 0; i--) {
+			if(values[i] >= minimunValue) {
+				lastIndex = i;
+				break;
+			}
+		}	
+		return lastIndex;
+	}
+	
+	private int[] listNumberOfValue(int[] values, int maxValues) {
+		int[] numberOfValue = new int[maxValues + 1];
+		for(int value : values) 
+			numberOfValue[value]++;
+		return numberOfValue;
+	}
+}
--- src/Yatzy.java
+++ src/Yatzy.java
@@ -1,115 +1,69 @@
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
+    public static int chance(DiceRoll aDiceRoll)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return aDiceRoll.sumDiceValue();
     }
 
-    public static int yatzy(int... dice)
+    public static int ones(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(1) * 1;
+    }
+
+    public static int twos(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(2) * 2;
+    }
+
+    public static int threes(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(3) * 3;
+    }
+    
+    public static int fours(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(4) * 4;
+    }
+    
+    public static int fives(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(5) * 5;
+    }
+    
+    public static int sixes(DiceRoll aDiceRoll) {
+        return aDiceRoll.numberOf(6) * 6;
+    }
+
+    
+    public static int score_pair(DiceRoll aDiceRoll)
     {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
+    	int score = 0;
+    	Integer dieValue = aDiceRoll.findHighestMatchingDice(2);
+    	if(dieValue != null)
+    		score = dieValue * 2;
+    	return score;
     }
-
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
-    }
-
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
-    }
-
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
-    }
-
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
+    
+    public static int three_of_a_kind(DiceRoll aDiceRoll)
     {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+    	int score = 0;
+    	Integer dieValue = aDiceRoll.findHighestMatchingDice(3);
+    	if(dieValue != null)
+    		score = dieValue * 3;
+    	return score;
     }
-
-    public int fours()
+    
+    public static int four_of_a_kind(DiceRoll aDiceRoll)
     {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+    	int score = 0;
+    	Integer dieValue = aDiceRoll.findHighestMatchingDice(4);
+    	if(dieValue != null)
+    		score = dieValue * 4;
+    	return score;
     }
-
-    public int fives()
+    
+    public static int yatzy(DiceRoll aDiceRoll)
     {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
-    }
-
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
-    }
-
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
+    	int score = 0;
+    	Integer dieValue = aDiceRoll.findHighestMatchingDice(5);
+    	if(dieValue != null)
+    		score = 50;
+    	return score;
     }
 
     public static int two_pair(int d1, int d2, int d3, int d4, int d5)
@@ -132,37 +86,7 @@ public class Yatzy {
         else
             return 0;
     }
-
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
-    }
-
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
-    }
-
+    
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
         int[] tallies;
